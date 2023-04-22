cdeff3ae5a0a17acb3adc422907fa0fe94e91b35
Rychkova Lyubov <rychkova.love@yandex.ru>
2023-04-22 13:09:28 +0300

    Fix indentation

--- info/create_table.sql
+++ info/create_table.sql
@@ -6,39 +6,39 @@ DROP TABLE data_carrier;
 DROP TABLE film;
 
 CREATE TABLE transaction (
-	id					serial,
+	id				serial,
 	client_id			serial,
 	carrier_id			serial,
 	film_title			varchar(100),
 	rent_price			real,
-	date_of_lease		date,
-	date_of_return  	date
+	date_of_lease			date,
+	date_of_return  		date
 );
 
 
 CREATE TABLE client (
-	id 					serial,
+	id 				serial,
 	full_name 			varchar(200),
 	address 			varchar(600),
-	phone_number 		varchar(11)
+	phone_number 			varchar(11)
 );
 
 
 CREATE TABLE data_carrier (
-	id					serial,
+	id				serial,
 	film_id				serial,
 	film_title			varchar(200),
 	type				carrier_type,
 	rent_price			real,
-	is_available		boolean
+	is_available			boolean
 
 );
 
 
 CREATE TABLE film (
-	id					serial,
+	id				serial,
 	title 				varchar(200),
-	production_company 	varchar(200),
+	production_company 		varchar(200),
 	director 			varchar(200),
-	year_of_release 	integer
+	year_of_release 		integer
 );
