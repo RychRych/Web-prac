a1cbb17b6adae1b42084e50c9e246e1849a8938a
Rychkova Lyubov <rychkova.love@yandex.ru>
2023-04-22 15:57:25 +0300

    ADD primary and foreign key to DB

--- info/create_table.sql
+++ info/create_table.sql
@@ -5,40 +5,37 @@ DROP TABLE client;
 DROP TABLE data_carrier;
 DROP TABLE film;
 
-CREATE TABLE transaction (
-	id				serial,
-	client_id			serial,
-	carrier_id			serial,
-	film_title			varchar(100),
-	rent_price			real,
-	date_of_lease			date,
-	date_of_return  		date
-);
-
 
 CREATE TABLE client (
-	id 				serial,
+	id 				serial		PRIMARY KEY,
 	full_name 			varchar(200),
 	address 			varchar(600),
 	phone_number 			varchar(11)
 );
 
-
-CREATE TABLE data_carrier (
-	id				serial,
-	film_id				serial,
-	film_title			varchar(200),
-	type				carrier_type,
-	rent_price			real,
-	is_available			boolean
-
-);
-
-
 CREATE TABLE film (
-	id				serial,
+	id				serial		PRIMARY KEY,
 	title 				varchar(200),
 	production_company 		varchar(200),
 	director 			varchar(200),
 	year_of_release 		integer
+);
+
+CREATE TABLE data_carrier (
+	id				serial		PRIMARY KEY,
+	film_id				serial		REFERENCES film (id),
+	film_title			varchar(200),
+	type				carrier_type,
+	rent_price			real,
+	is_available			boolean
+);
+
+CREATE TABLE transaction (
+	id				serial		PRIMARY KEY,
+	client_id			serial		REFERENCES client (id),
+	carrier_id			serial		REFERENCES data_carrier (id),
+	film_title			varchar(100),
+	rent_price			real,
+	date_of_lease			date,
+	date_of_return  		date
 );
