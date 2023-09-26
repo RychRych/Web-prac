2e520bdc2a3f842369306b8ec111379698e52664
lyubov <rychkova.love@yandex.ru>
2023-09-26 22:18:48 +0300

    Add filters to FilmDAO

--- src/main/java/webprac/webprac/DAO/FilmDAO.java
+++ src/main/java/webprac/webprac/DAO/FilmDAO.java
@@ -5,4 +5,7 @@ import webprac.webprac.models.Film;
 import java.util.List;
 
 public interface FilmDAO extends GenericDAO<Film>{
+    List<Film> getByTitle(String name);
+
+    List<Film> getByYear(Integer year);
 }
--- src/main/java/webprac/webprac/DAO/impl/FilmDAOImpl.java
+++ src/main/java/webprac/webprac/DAO/impl/FilmDAOImpl.java
@@ -1,9 +1,13 @@
 package webprac.webprac.DAO.impl;
 
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 import webprac.webprac.DAO.FilmDAO;
 import webprac.webprac.models.Film;
+import webprac.webprac.models.Transaction;
 
+import java.util.List;
 
 
 @Repository
@@ -12,4 +16,27 @@ public class FilmDAOImpl extends GenericDAOImpl<Film> implements FilmDAO {
         super(Film.class);
     }
 
+    @Override
+    public List<Film> getByTitle(String title) {
+        try (Session session = sessionFactory.openSession()) {
+            NativeQuery<Film> query = session.createNativeQuery(
+                    "SELECT * FROM film WHERE title LIKE :title",
+                    Film.class
+            ).setParameter("title", title);
+            query.getResultList();
+            return query.list();
+        }
+    }
+
+    @Override
+    public List<Film> getByYear(Integer year) {
+        try (Session session = sessionFactory.openSession()) {
+            NativeQuery<Film> query = session.createNativeQuery(
+                    "SELECT * FROM film WHERE year_of_release = :year",
+                    Film.class
+            ).setParameter("year", year);
+            query.getResultList();
+            return query.list();
+        }
+    }
 }
