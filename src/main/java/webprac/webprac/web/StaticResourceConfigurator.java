82d1775348107bd89b7ff947f3418ff375dc22b1
lyubov <rychkova.love@yandex.ru>
2023-09-28 00:51:29 +0300

    Add beautiful css styling sponsored by seiko srpd51

---
+++ src/main/java/webprac/webprac/web/StaticResourceConfigurator.java
@@ -0,0 +1,15 @@
+package webprac.webprac.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class StaticResourceConfigurator implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("file:./src/main/resources/static/");
+    }
+}
